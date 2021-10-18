3a697399dd08e0c9ec8c2eb24187628b49f2d174
alzybaad <alzybaad@gmail.com>
2021-10-18 16:18:14 +0900

    upgrade Gradle & Android Studio versions

--- example/src/main/java/team/birdhead/licenseview/example/ExampleActivity.java
+++ example/src/main/java/team/birdhead/licenseview/example/ExampleActivity.java
@@ -1,7 +1,8 @@
 package team.birdhead.licenseview.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ExampleActivity extends AppCompatActivity {
 
